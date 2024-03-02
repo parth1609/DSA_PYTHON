6806e7bf1a017db49c6113896dcce49edbeb70b4
parth1609 <115941606+parth1609@users.noreply.github.com>
2024-03-03 00:58:49 +0530

    Add files via upload

--- graph_list.py
+++ graph_list.py
@@ -12,8 +12,8 @@ class Graph:
     
     def rmov_edge(self,u,v):
         if 0<=u<self.vertex and 0<=v<self.vertex:
-            self.adj_list[u] = [(vertex,weight) for vertex,weight in self.adj_list(u) if vertex!= v]
-            self.adj_list[v] = [(vertex,weight) for vertex,weight in self.adj_list(u) if vertex!= u]
+            self.adj_list[u] = [(vertex,weight) for vertex,weight in self.adj_list[u] if vertex != v]
+            self.adj_list[v] = [(vertex,weight) for vertex,weight in self.adj_list[v] if vertex!= u]
             
         else:
             print("sorry")
@@ -35,6 +35,7 @@ g.add_edge(1,2)
 g.add_edge(1,3)
 g.add_edge(2,4)
 g.add_edge(3,4)
+g.rmov_edge(1,3)
 
 g.print_list()
 
